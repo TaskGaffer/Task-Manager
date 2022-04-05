131eaf9c1da4050f44207d3230f1a84b1c947c59
HeyoRay <raymond.huang1011@gmail.com>
2022-04-05 16:47:27 -0400

    functional input field

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,49 @@
-// const db = require('../models/model)
+const db = require('../models/model');
 
 const userController = {};
 
 // post new user
-userController.newUser = (req, res, next) => {};
+userController.newUser = async (req, res, next) => {
+  const query = {
+    text: `INSERT INTO users (user_name, password) VALUES($1, $2) RETURNING _id;`,
+    values: [req.body.user_name, req.body.password],
+  };
+  try {
+    // await database query
+    const response = await db.query(query.text, query.values);
+    console.log(response.rows[0]._id);
+    const user_id = response.rows[0]._id;
+    res.locals.id = user_id;
+    return next();
+  } catch (err) {
+    // do something w/ err
+    return next({
+      log: `Cannot insert new user. ERROR: ${err}`,
+      message: { err: 'Error occurred in userController.newUser' },
+    });
+  }
+};
 
 //  user login
-userController.login = (req, res, next) => {};
+userController.login = async (req, res, next) => {
+  const query = {
+    text: `SELECT _id FROM users WHERE user_name = $1 AND password = $2;`,
+    values: [req.body.user_name, req.body.password],
+  };
+  try {
+    // await database query
+    const response = await db.query(query.text, query.values);
+    console.log(response.rows[0]._id);
+    const user_id = response.rows[0]._id;
+    res.locals.id = user_id;
+    return next();
+  } catch (err) {
+    // do something w/ err
+    return next({
+      log: `Cannot login user. ERROR: ${err}`,
+      message: { err: 'Error occurred in userController.login' },
+    });
+  }
+};
 
 module.exports = userController;
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 const InputField = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [errorStatus, setErrorStatus] = useState(false)
+
+  const inputChangeHandler = (e) => {
+    setErrorStatus(false)
+    setInputValue(e.target.value)
+  }
+
+  const submitHandler = () => {
+    if (inputValue.trim().length === 0) {
+      setInputValue('')
+      setErrorStatus(true);
+    } else {
+      console.log(inputValue);
+      setInputValue('')
+    }
+  }
+
+  const keyPressHandler = (e) => {
+    if (e.key === 'Enter') {
+      submitHandler()
+    }
+  }
+
+
   return (
-    <div></div>
+    <div className='inputField'>
+      <TextField
+        className='inputBox'
+        error={errorStatus}
+        id={errorStatus ? "outlined-error-helper-text" : "outlined-basic"}
+        label={errorStatus ? "Input Valid Heading" : "Input Heading"}
+        variant="outlined"
+        value={inputValue}
+        onChange={inputChangeHandler}
+        onKeyPress={keyPressHandler}
+      />
+      <Button
+        className='inputSubmit'
+        variant="outlined"
+        color={errorStatus ? "error" : "primary"}
+        onClick={submitHandler}
+      >
+        <AddIcon />
+      </Button>
+    </div>
   )
 }
 
